e8472b03c5185292df678396831e361ce3ab7162
Abinash <abinash261997@gmail.com>
2022-01-27 15:24:21 +0530

    using of class component

---
+++ src/User.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+class User extends Component {
+    render() {
+        // render is compulsory in class component while return is compulsory in return component
+        return <h1>Class Component from user file</h1>;
+    }
+}
+export default User;
