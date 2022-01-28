912030f28e87f80cadcfe5201dc7ffa95550fcf7
Abinash <abinash261997@gmail.com>
2022-01-28 14:26:35 +0530

    condential rendering in recomended way

--- src/Profile.js
+++ src/Profile.js
@@ -3,17 +3,15 @@ import React, { useState } from "react";
 function Profile() {
     // const [loggedIn, setLoggedIn] = useState(false); // <--------- if caredental incorrect //
     const [loggedIn, setLoggedIn] = useState(true); // <--------- if caredental correct //
-    //not recommended way of using conditional rendering
+    //recommended way of using conditional rendering
     if (loggedIn) {
         return (
             <div>
-                <h1>Logged Successfully</h1>
-            </div>
-        );
-    } else {
-        return (
-            <div>
-                <h1>Invalid User</h1>
+                {loggedIn ? (
+                    <h1>Logged Successfully</h1>
+                ) : (
+                    <h1>login Failed</h1>
+                )}
             </div>
         );
     }
